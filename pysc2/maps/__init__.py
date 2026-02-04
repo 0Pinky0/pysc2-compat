eb789f2122c423ef446b5275405aa96595aa04bb
0Pinky0 <lpinkyl@126.com>
2026-02-04 15:49:44 +0800

    Rename test scripts.

--- pysc2/maps/__init__.py
+++ pysc2/maps/__init__.py
@@ -14,9 +14,9 @@
 """Register/import the maps, and offer a way to create one by name.
 
 Users of maps should import this module:
-  from pysc2 import maps
+    from pysc2 import maps
 and create the maps by name:
-  maps.get("MapName")
+    maps.get("MapName")
 
 If you want to create your own map, then import the map lib and subclass Map.
 Your subclass will be implicitly registered as a map that can be constructed by
